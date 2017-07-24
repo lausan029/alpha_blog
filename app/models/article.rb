1fe00b98666e4d4d10bf6e1ddcdead6a7e41839d
Laura Sanchez <lau.san029@gmail.com>
2017-07-24 17:03:36 -0400

    create and show articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title, presence:  true, length: { minimim: 3, maximun:50}
-	validates :description, presence:  true, length: { minimim: 3, maximun:50}
+	validates :title, presence:  true
+#	validates :title, presence:  true, length: { minimim: 3, maximun:50}
+#	validates :description, presence:  true, length: { minimim: 3, maximun:50}
 end
