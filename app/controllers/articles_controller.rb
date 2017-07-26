72c52ed1e1ff185a5fc9c80714e7934e7ff3c82a
Laura Sanchez <lau.san029@gmail.com>
2017-07-26 15:42:56 -0400

    dont repeat your self

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def index
 		@articles = Article.all
 	end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def create
@@ -24,7 +25,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Modificado correctamente"
 			redirect_to article_path(@article)
@@ -33,10 +33,20 @@ class ArticlesController < ApplicationController
 		end
 	end
 	def show 
-		@article = Article.find(params[:id])
+
+	end
+
+	def destroy
+		@article.destroy
+		flash[:notice] = "Articulo eliminado"
+		redirect_to articles_path
 	end
 
 	private
+	def set_article
+		@article = Article.find(params[:id])	
+	end
+
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
