6e25aa90fc0df5aaa6f2f44af289ecb4cba01595
Laura Sanchez <lau.san029@gmail.com>
2017-07-28 17:19:08 -0400

    debug and association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
+	belongs_to :user
 	validates :title, presence:  true
-#	validates :title, presence:  true, length: { minimim: 3, maximun:50}
+	validates :user_id, presence:  true
+#	validates :title, presence:  true, length: { minimum: 3, maximum:50}
 #	validates :description, presence:  true, length: { minimim: 3, maximun:50}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728142635) do
+ActiveRecord::Schema.define(version: 20170728201550) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "update_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
